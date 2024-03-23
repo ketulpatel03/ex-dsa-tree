bcfd0664d7bf242d23a318a465e9e7ffc49c9648
ketulpatel03 <ketulpatel03@yahoo.co.in>
2024-03-22 22:02:41 -0600

    first commit

---
+++ src/main/java/com/java/TreeImplementation.java
@@ -0,0 +1,19 @@
+package com.java;
+
+public class TreeImplementation {
+
+    public static void main(String[] args) {
+
+        TreeNode tree1 = new TreeNode(1);
+        tree1.left = new TreeNode(2);
+        tree1.right = new TreeNode(3);
+        tree1.left.left = new TreeNode(4);
+        tree1.left.right = new TreeNode(5);
+        tree1.right.left = new TreeNode(6);
+        tree1.right.right = new TreeNode(7);
+
+        System.out.println(tree1);
+
+    }
+
+}
